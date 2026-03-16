71d487ef60346b0fc40632ee9e1e8132692fa0a0
xckus <xckushal@gmail.com>
2026-03-16 22:33:33 +0530

    Added the sleep timer

--- app/src/main/java/com/kushalgangadhar/sling/ui/screens/NowPlayingScreen.kt
+++ app/src/main/java/com/kushalgangadhar/sling/ui/screens/NowPlayingScreen.kt
@@ -24,7 +24,6 @@ import kotlin.math.hypot
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.kushalgangadhar.sling.utils.TimerHelper
